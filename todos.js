424420279fcff8a53a7e434fd35aa15c64956cfb
Joshua Morgan <jmorg0605@gmail.com>
2023-03-18 13:18:03 -0400

    initialized static/seed data for intial testing

--- todos.js
+++ todos.js
@@ -5,14 +5,18 @@ const app = express();
 const host = 'localhost';
 const port = 3000;
 
+// Static data for initial testing
+let todoLists = require('./lib/seed-data');
+
 app.set('views', './views'); // where to find views templates
 app.set('view engine', 'pug'); // tells to use Pug as view engine
 
 app.use(morgan('common')); // enable logging with morgan
+app.use(express.static('public'));
 
 app.get('/', (req, res, next) => {
-  res.render('lists')
-})
+  res.render('lists');
+});
 
 app.listen(port, host, () => {
   console.log(`listening on port ${port}`);
