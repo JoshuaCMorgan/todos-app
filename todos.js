fceea3b0fd1dd39725f52094826c4fb4ad23517e
Joshua Morgan <jmorg0605@gmail.com>
2023-03-18 12:44:23 -0400

    initial express program

---
+++ todos.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+const host = 'localhost';
+const port = 3000;
+
+app.set('views', './views'); // where to find views templates
+app.set('views engine', 'pug'); // tells to use Pug as view engine
+
+app.use('morgan'); // enable logging with morgan
+
+app.get('/', (req, res, next) => {
+  res.render('list')
+})
+
+app.listen(port, host, () => {
+  console.log(`listening on port ${port}`);
+});
